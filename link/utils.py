c94259a5a39f06f9aa3695997fb89072ff5b548a
EmptyRabbit <qiuzhouxin@163.com>
2019-11-27 00:13:47 +0800

    finish link base

--- link/utils.py
+++ link/utils.py
@@ -18,11 +18,11 @@ class SingleLink():
         Get the value of the index-th node in the linked list. If the index is invalid, return -1.
         index start with 0
         """
-        if index < 0 or index > self.length:
+        if index < 0 or index > self.length - 1:
             return -1
 
         result = _next = self.head
-        for _ in range(index):
+        for _ in range(index + 1):
             result = _next
             _next = _next.next_node
 
@@ -40,12 +40,17 @@ class SingleLink():
         """
         Append a node of value val to the last element of the linked list.
         """
+        if self.length <= 0:
+            self.add_head(val)
+            return
+
         result = _next = self.head
         for _ in range(self.length):
             result = _next
             _next = _next.next_node
 
         result.next_node = LinkNode(val, None)
+        self.length += 1
 
     def add_at_index(self, index: int, val: int) -> None:
         """
@@ -53,3 +58,38 @@ class SingleLink():
         If index equals to the length of linked list, the node will be appended to the end of linked list.
         If index is greater than the length, the node will not be inserted.
         """
+        if index > self.length:
+            return
+
+        if index <= 0:
+            self.add_head(val)
+            return
+
+        result = _next = self.head
+        for _ in range(index):
+            result = _next
+            _next = _next.next_node
+
+        result.next_node = LinkNode(val, _next)
+
+        self.length += 1
+
+    def delete_index(self, index: int) -> None:
+        """
+        Delete the index-th node in the linked list, if the index is valid.
+        """
+        if index < 0 or index > self.length - 1:
+            return
+
+        if index == 0:
+            self.head = self.head.next_node
+            self.length -= 1
+            return
+
+        result = _next = self.head
+        for _ in range(index):
+            result = _next
+            _next = _next.next_node
+
+        result.next_node = _next.next_node
+        self.length -= 1
