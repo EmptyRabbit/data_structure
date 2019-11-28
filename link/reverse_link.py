84479873d80104199ddb31188b4480a364dce34c
EmptyRabbit <qiuzhouxin@163.com>
2019-11-28 12:26:50 +0800

    repractice reverse link

--- link/reverse_link.py
+++ link/reverse_link.py
@@ -1,51 +1,25 @@
-'''
+"""
 链表反转
 思路：原来指向后一个节点，现改为指向前一个节点，再变更头节点（原头节点指向尾节点后置null）
 1、保存当前节点的next节点
 2、当前next指向当前的前一个
 3、变更当前节点为next
 图解：https://blog.csdn.net/xyh269/article/details/70238501
-'''
-from single_link import SingleLinkList
+"""
 
 
-class Node():
-    '''链表节点'''
+def reverse_single_link(head):
+    """
+    #206
+    :param head:
+    :return:
+    """
+    pre_node = None
+    while head:
+        tmp = head.next_node
+        head.next_node = pre_node  # head next指向前一个
+        pre_node = head  # 下一个pre_node指向当前的head，此时head next已指向前一个
 
-    def __init__(self, val, next_=None):
-        self.val = val
-        self.next_ = next_
+        head = tmp
 
-
-class Solution:
-    '''
-        1->2->3->4
-        4->3->2->1
-    '''
-
-    @staticmethod
-    def reverseList(head: Node) -> Node:
-        pre = None
-        new_head = None
-
-        while(head):
-            next_ = head.next_
-            head.next_ = pre
-            new_head = head
-            pre = head
-            head = next_
-
-        return new_head
-
-
-if __name__ == '__main__':
-    test = SingleLinkList()
-    test.addAtHead(1)
-    test.addAtIndex(1, 2)
-    test.addAtIndex(2, 3)
-    test.addAtIndex(3, 4)
-    test.addAtIndex(4,5)
-    print(test)
-
-    test.head = Solution.reverseList(test.head)
-    print(test)
+    return pre_node
--- link/utils.py
+++ link/utils.py
@@ -3,10 +3,18 @@ class LinkNode():
         self.val = val
         self.next_node = _next
 
+    def to_list(self):
+        result = []
+        _next_node = self
+        while _next_node:
+            result.append(self.val)
+            _next_node = self.next_node
+        return result
+
 
 class SingleLink():
     """
-    #707设计链表的操作
+    #707 设计链表的操作
     """
 
     def __init__(self):
@@ -15,8 +23,7 @@ class SingleLink():
 
     def get(self, index: int) -> int:
         """
-        Get the value of the index-th node in the linked list. If the index is invalid, return -1.
-        index start with 0
+        获取链表index节点。index从0开始
         """
         if index < 0 or index > self.length - 1:
             return -1
@@ -30,15 +37,14 @@ class SingleLink():
 
     def add_head(self, val: int) -> None:
         """
-        Add a node of value val before the first element of the linked list.
-        After the insertion, the new node will be the first node of the linked list.
+        添加头节点
         """
         self.head = LinkNode(val, self.head)
         self.length += 1
 
     def add_tail(self, val: int) -> None:
         """
-        Append a node of value val to the last element of the linked list.
+        添加尾节点，如果链表长度为0，添加头节点
         """
         if self.length <= 0:
             self.add_head(val)
@@ -54,9 +60,10 @@ class SingleLink():
 
     def add_at_index(self, index: int, val: int) -> None:
         """
-        Add a node of value val before the index-th node in the linked list.
-        If index equals to the length of linked list, the node will be appended to the end of linked list.
-        If index is greater than the length, the node will not be inserted.
+        在index个节点前插入，index从0开始
+        index等于链表长度时，插入为尾节点
+        index等于链表长度，无任何操作
+        index小于等于0，插入为头节点
         """
         if index > self.length:
             return
@@ -71,12 +78,11 @@ class SingleLink():
             _next = _next.next_node
 
         result.next_node = LinkNode(val, _next)
-
         self.length += 1
 
     def delete_index(self, index: int) -> None:
         """
-        Delete the index-th node in the linked list, if the index is valid.
+        删除index节点，index从0开始
         """
         if index < 0 or index > self.length - 1:
             return
